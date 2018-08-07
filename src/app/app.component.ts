47a34f93372999a6285c8027d6478a44638c186c
pmcally <pmcnally32@gmail.com>
2018-08-07 13:27:35 -0500

    Full Upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Abanoub-firstOne';
+  todoArray=[];
+
+  addTodo(value) {
+    if (value == "") {
+      console.log(" Blank Record");
+    } else {
+    this.todoArray.push(value)
+    }
+    console.log(this.todoArray)
+  }
+
+  deleteItem(todo) {
+    for(let i=0 ;i<= this.todoArray.length ;i++) {
+      if(todo == this.todoArray[i]) {
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+}
